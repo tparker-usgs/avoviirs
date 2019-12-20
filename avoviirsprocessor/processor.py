9c21e1d61b9f9599d54b88ccb3f7f83831f3cd98
Tom Parker <tparker@usgs.gov>
2019-12-19 15:09:14 -0900

    add logging

--- avoviirsprocessor/processor.py
+++ avoviirsprocessor/processor.py
@@ -88,6 +88,7 @@ def publish_product(filename, pngimg, volcview_args):
         url = endpoint + "/imageApi/uploadImage"
         print("publishing image to {}".format(url))
         print("data {}".format(volcview_args))
+        print(f"file size {len(pngimg)}")
         try:
             response = requests.post(
                 url,
